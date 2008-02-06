92d7e6b93573d6c345528e4b4a3e0bf81bfeb1be
paultcochrane <paultcochrane@a0a4e76c-ba24-0410-a01c-b269113d594b>
2008-02-06 22:06:25 +0000

    Added a setup python script for installation as a normal python module.  We need to do some python-related structural reorganisation before this script will run correctly.  Graham: I'll send you an email about this when I get the tuits and think about what needs to be done a bit more.
    git-svn-id: https://xmds.svn.sourceforge.net/svnroot/xmds/trunk/xpdeint@1762 a0a4e76c-ba24-0410-a01c-b269113d594b

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+a=setup(name="xpdeint",
+      version="0.0.1",
+      description="Postscript Graphics with Python",
+      author="Graham Dennis",
+      author_email="grahamdennis@users.sourceforge.net",
+      maintainer="Graham Dennis",
+      maintainer_email="grahamdennis@users.sourceforge.net",
+      url="http://xmds.sourceforge.net",
+      license="GPL",
+      keywords="scientific/engineering simulation",
+      platforms="OS Independent",
+      packages=['xmds2'],
+)
