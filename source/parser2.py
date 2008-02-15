559d2121310916f14b319b75a40628d5f26f5189
grahamdennis <grahamdennis@a0a4e76c-ba24-0410-a01c-b269113d594b>
2008-02-15 02:57:41 +0000

    Comments are good.
    git-svn-id: https://xmds.svn.sourceforge.net/svnroot/xmds/trunk/xpdeint@1779 a0a4e76c-ba24-0410-a01c-b269113d594b

--- source/parser2.py
+++ source/parser2.py
@@ -18,18 +18,37 @@ if sys.platform == 'darwin':
   import new
   sys.modules['WebKit'] = new.module('WebKit')
 
+
+# Import the parser stuff
 from ParserException import ParserException
 from XMDS2Parser import XMDS2Parser
 
+# Import the top level template
 from Simulation import Simulation as SimulationTemplate
 
+# Import the IndentFilter. The IndentFilter is the magic filter
+# that when used correctly makes the generated source correctly
+# indented. See the comments in IndentFilter.py for more info.
 from IndentFilter import IndentFilter
 
+
+# The help message printed when --help is used as an argument
 help_message = '''
 The help message goes here.
 '''
 
+
+# These are additional functions that will be added to the XML
+# classes for convenience.
 def getChildElementsByTagName(self, tagName, optional=False):
+  """
+  Return child XML elements that have the tag name `tagName`.
+  
+  This function will raise an exception if there are no children
+  with `tagName` unless the keyword argument `optional` is passed
+  with a value of `True`.
+  """
+  
   elements = filter(lambda x: x.nodeType == minidom.Node.ELEMENT_NODE and x.tagName == tagName, self.childNodes)
   if not optional and len(elements) == 0:
     raise ParserException(self, "There must be at least one '" + tagName + "' element.")
@@ -37,6 +56,14 @@ def getChildElementsByTagName(self, tagName, optional=False):
 
 
 def getChildElementByTagName(self, tagName, optional=False):
+  """
+  Return the child XML element that has the tag name `tagName`.
+  
+  This function will raise an exception if there is more than
+  one child with tag name `tagName`. Unless the keyword argument
+  `optional` is passed with a value of `True`, an exception will
+  also be raised if there is no child with tag name `tagName`.
+  """
   elements = self.getChildElementsByTagName(tagName, optional=optional)
 
   if not optional and not len(elements) == 1:
@@ -52,6 +79,21 @@ def getChildElementByTagName(self, tagName, optional=False):
 
 
 def innerText(self):
+  """
+  Return the contents of all descendent text nodes and CDATA nodes.
+  
+  For example, consider the following XML::
+  
+  <root>
+    <someTag> someText </someTag>
+    <![CDATA[
+      some other text.
+    ]]>
+  </root>
+  
+  On the `root` element, `innerText` would return " someText some other text." plus
+  or minus some newlines and other whitespace.
+  """
   contents = []
   for child in self.childNodes:
     if child.nodeType == minidom.Node.ELEMENT_NODE:
@@ -62,6 +104,9 @@ def innerText(self):
 
 
 def cdataContents(self):
+  """
+  Return the contents of any CDATA nodes that are children of this element.
+  """
   contents = []
   for child in self.childNodes:
     if child.nodeType == minidom.Node.CDATA_SECTION_NODE:
@@ -69,19 +114,34 @@ def cdataContents(self):
   return "".join(contents)
 
 
-# Function to return an (any) object from an iterable, though designed to be used with sets
 def anyObject(iterable):
+  """
+  Return an object from an iterable. This is designed to be used with sets
+  because I can't work out any other way of doing this, but it will work
+  with any iterable.
+  """
   for obj in iterable:
     return obj
 
 
 class Usage(Exception):
+  """
+  Exception class used when an error occurs parsing command
+  line arguments.
+  """
   def __init__(self, msg):
     self.msg = msg
 
 
 def main(argv=None):
+  
+  # Default to not being verbose with error messages
+  # If verbose is true, then when an error occurs during parsing,
+  # the Python backtrace will be shown in addition to the XML location
+  # where the error occurred.
   verbose = False
+  
+  # Attempt to parse command line arguments
   if argv is None:
     argv = sys.argv
   try:
@@ -104,8 +164,11 @@ def main(argv=None):
     print >> sys.stderr, "\t for help use --help"
     return 2
   
+  # FIXME: Work out the simulation script that we read from command-line arguments
   scriptName = "../examples/kubo.xmds"
   
+  
+  # Add the helper methods defined earlier to the XML classes
   minidom.Element.getChildElementsByTagName  = getChildElementsByTagName
   minidom.Document.getChildElementsByTagName = getChildElementsByTagName
   
@@ -115,15 +178,25 @@ def main(argv=None):
   minidom.Element.innerText = innerText
   minidom.Element.cdataContents = cdataContents
   
+  
+  # globalNameSpace is a dictionary of variables that are available in all
+  # templates
   globalNameSpace = {}
   
+  
+  # Open the script file
   scriptFile = file(scriptName)
+  # Read the contents of the file
   globalNameSpace['inputScript'] = scriptFile.read()
+  # Close the file
   scriptFile.close()
   del scriptFile
   
+  # Parse the XML input script into a set of XML
+  # classes
   xmlDocument = minidom.parse(scriptName)
   
+  # Set up the globalNameSpace with the appropriate variables
   globalNameSpace['xmlDocument'] = xmlDocument
   globalNameSpace['scriptElements'] = []
   globalNameSpace['features'] = {}
@@ -135,23 +208,40 @@ def main(argv=None):
                              'subversionRevision': 'rF00'}
   globalNameSpace['templates'] = set()
   
+  # We need the anyObject function in a few templates, so
+  # we add it to the globalNameSpace, so that the function can
+  # be called from a template like $anyObject(someContainer)
   globalNameSpace['anyObject'] = anyObject
   
+  # Now start the process of parsing the XML class structure
+  # onto a template heirarchy.
   try:
     parser = None
+    
+    # Check if the XML claims to be an XMDS2 script
     if XMDS2Parser.canParseXMLDocument(xmlDocument):
       parser = XMDS2Parser()
     
+    # We don't have a parser that understands the XML, so we must bail
     if not parser:
       print >> sys.stderr, "Unable to recognise file as an xmds script."
       return
     
+    # Set our magic filter
     filterClass = IndentFilter
+    # Construct the top-level template class
     simulationTemplate = SimulationTemplate(searchList=[globalNameSpace], filter=filterClass)
+    # Add it to the list of scriptElements
     globalNameSpace['scriptElements'].append(simulationTemplate)
+    # Now get the parser to do the complex job of mapping the XML classes onto our
+    # templates.
     simulationTemplate.simulation = parser.parseXMLDocument(xmlDocument, globalNameSpace, filterClass)
     
     # Now run preflight stage
+    # Preflight is the stage which maps vector names, etc to the actual vectors themselves. It also
+    # allows all templates to check that all of their settings are sane, and raise an exception if
+    # there is a problem.
+    #
     # FIXME: While this works, I don't believe it is the best design
     # Instead of having these somewhat arbitrary 'scriptElements', they should all be set
     # as children of the SimulationElement, and the SimulationElement should be set as a child of
@@ -181,9 +271,21 @@ def main(argv=None):
       preflights = delayedPreflights.copy()
       delayedPreflights = set()
     
+    # Preflight is done
+    
+  # If there was an exception during parsing or preflight, a ParserException should have been
+  # raised. The ParserException knows the XML element that triggered the exception, and the 
+  # error string that should be presented to the user.
   except ParserException, err:  
     elementPath = []
     currentElement = err.element
+    
+    # Construct an XPath-like string (but more user-friendly) which allows the user to 
+    # locate the XML element that caused the exception. I'd use line/column numbers too
+    # except that that information isn't available as part of the XML spec, so there isn't
+    # any function that you can all on an element to get that information. At least from
+    # what I can tell.
+    
     while 1:
       if not currentElement:
         break
@@ -203,17 +305,27 @@ def main(argv=None):
           elementDescription += '[' + str(siblingElements.index(currentElement)) + ']'
       elementPathString = elementPathString + "'" + elementDescription + "'"
     
+    # Print the error to the user
     print >> sys.stderr, "Error: " + err.msg
     print >> sys.stderr, "    In element: " + elementPathString
     print >> sys.stderr, "For a complete traceback, pass -v on the command line."
     
+    # If we have the verbose option on, then in addition to the path to the XML element
+    # that triggered the exception, print a traceback showing the list of Python function
+    # calls that led to the exception being hit.
     if verbose:
       raise
     
     return -1
   
+  # We don't need the 'vectors' variable any more.
   del globalNameSpace['vectors']
   
+  # FIXME: Currently we just print the generated source to standard out.
+  # What we should do is output the generated source to a file.
+  # The name of the file should come from the <name> tag of the XMDS
+  # script. This variable is available from the globalNameSpace using
+  # globalNameSpace['simulationName'].
   print simulationTemplate
 
 
