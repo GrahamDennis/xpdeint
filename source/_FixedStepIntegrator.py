7736bd61e48bf53736f3215e3c979f30abb08797
grahamdennis <grahamdennis@41b19578-b77a-4e3b-9682-c8570d237623>
2008-03-01 02:24:52 +0000

    xpdeint: Preparations for cross_propagation.
    The FixedStepIntegrator now knows that it may be capable of being used as a
    cross-propagation integrator, and prevents the ErrorCheck feature from subdividing
    the step-size in cross-propagation.
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/xmds/code/trunk/xpdeint@1845 41b19578-b77a-4e3b-9682-c8570d237623

---
+++ source/_FixedStepIntegrator.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+_FixedStepIntegrator.py
+
+Created by Graham Dennis on 2008-03-01.
+Copyright (c) 2008 __MyCompanyName__. All rights reserved.
+"""
+
+from Integrator import Integrator
+
+class _FixedStepIntegrator (Integrator):
+  # Is this integrator capable of doing cross-propagation integration?
+  isCrossCapable = False
+  
+  def __init__(self, *args, **KWs):
+    Integrator.__init__(self, *args, **KWs)
+    
+    # Set default variables
+    self._cross = False
+    
+    # Is this integrator being used as a cross-propagation integrator?
+    self.cross = KWs.get('cross', False)
+    
+  
+  def _getCross(self):
+    return self._cross
+  
+  def _setCross(self, value):
+    if value and not self.isCrossCapable:
+      raise AssertionError("This class (%s) does not support cross-propagation." % str(type(self)))
+    else:
+      self._cross = value
+  
+  cross = property(_getCross, _setCross)
+  
