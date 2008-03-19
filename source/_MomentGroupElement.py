67b4e19c0db11b10b615943c1f7dd3e27ec86559
grahamdennis <grahamdennis@a0a4e76c-ba24-0410-a01c-b269113d594b>
2008-03-19 09:29:53 +0000

    xpdeint: I stuffed the last commit. Accidentally removed _MomentGroupElement.py. And forgot some makefiles.
    git-svn-id: https://xmds.svn.sourceforge.net/svnroot/xmds/trunk/xpdeint@1897 a0a4e76c-ba24-0410-a01c-b269113d594b

---
+++ source/_MomentGroupElement.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+_MomentGroupElement.py
+
+This contains all the pure-python code for MomentGroupElement.tmpl
+
+Created by Graham Dennis on 2007-10-18.
+Copyright (c) 2007 __MyCompanyName__. All rights reserved.
+"""
+
+from Geometry.FieldElement import FieldElement
+from ParserException import ParserException
+
+class _MomentGroupElement (FieldElement):
+  def __init__(self, number, *args, **KWs):
+    self.number = number
+    
+    FieldElement.__init__(self, *args, **KWs)
+    
+    # Set default variables
+    self.requiresInitialSample = False
+    self.getVar('momentGroups').append(self)
+    self.computedVectors = set()
+    self.operatorContainers = []
+  
+  @property
+  def children(self):
+    result = set(super(_MomentGroupElement, self).children)
+    result.update(self.computedVectors)
+    result.update(self.operatorContainers)
+    return result
+  
+  # The name of the moment group (used for the vector names, etc)
+  @property
+  def name(self):
+    return 'mg' + str(self.number)
+  
+  # Do we actually need to allocate the moment group vector?
+  # We may not need to allocate the raw vector if there is no
+  # processing of the raw vector to be done before it is written.
+  @property
+  def rawVectorNeedsToBeAllocated(self):
+    # If we have processing code, then we definitely need a raw vector
+    if self.hasattr('processingCode') and self.processingCode:
+      return True
+    
+    dict = {'returnValue': False, 'MomentGroup': self}
+    featureOrdering = ['Driver']
+    
+    # This function allows the features to determine whether or not the raw vector
+    # needs to be allocated by changing the value of the 'returnValue' key in dict.
+    # The features should only change the value to true if they need the raw vector
+    # allocated. Otherwise, they shouldn't touch the value.
+    self.insertCodeForFeatures('rawVectorNeedsToBeAllocated', featureOrdering, dict)
+    
+    return dict['returnValue']
+  
+  def bindNamedVectors(self):
+    super(_MomentGroupElement, self).bindNamedVectors()
+    
+    dependencies = self.vectorsFromEntity(self.dependenciesEntity)
+    
+    if self.hasattr('sampleField'):
+      sampleSpaceMask = self.sampleField.spaceMask
+    else:
+      sampleSpaceMask = self.spaceMask
+    
+    for dependency in dependencies:
+      if self.hasPostProcessing and dependency.type == 'complex':
+        self.rawVector.type = 'complex'
+      
+    
+    self.dependencies.update(dependencies)
+  
+
+
