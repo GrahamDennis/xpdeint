9f7ca78d93256ec0878ab38c987163f67bc86bbc
andyferris <andyferris@41b19578-b77a-4e3b-9682-c8570d237623>
2008-03-25 12:45:26 +0000

    More C-expression parsing abilities.
    * Now domains of continuous dimensions may be specified by a non-number with 
      run-time validation code.
    * The automated domain-parsing function had to be stolen from ScriptParser to
      mangle it to be more forgiving.
    
    
    git-svn-id: svn+ssh://svn.code.sf.net/p/xmds/code/trunk/xpdeint@1919 41b19578-b77a-4e3b-9682-c8570d237623

--- xpdeint/ScriptParser.py
+++ xpdeint/ScriptParser.py
@@ -31,39 +31,3 @@ class ScriptParser(object):
     for attrName, attrValue in attrDict.iteritems():
       obj.__setattr__(attrName, attrValue)
     
-  
-  def domainPairFromString(self, domainString, numberType, element):
-    """
-    Parse a string of the form ``(someNumber1, someNumber2)`` and return the two
-    strings corresponding to the numbers ``someNumber1`` and ``someNumber2`` making
-    sure that ``someNumber1`` is less than ``someNumber2``.
-    
-    `numberType` is the class to which the strings ``someNumber1`` and ``someNumber2`` will
-    be converted to ensure the ordering of the numbers.
-    """
-    
-    regex = re.compile(RegularExpressionStrings.domainPair)
-    result = regex.match(domainString)
-    if not result:
-      raise ParserException(element, "Could not understand '%(domainString)s' as a domain"
-                                     " of the form ( +/-someNumber, +/-someNumber)" % locals())
-    
-    minimumString = result.group(1)
-    maximumString = result.group(2)
-    
-    def validateNumberString(string):
-      try:
-        value = numberType(string)
-      except ValueError, err:
-        raise ParserException(element, "Could not understand '%(string)s' as a number." % locals())
-    
-    
-    validateNumberString(minimumString)
-    validateNumberString(maximumString)
-    
-    if numberType(minimumString) >= numberType(maximumString):
-      raise ParserException(element, "The end point of the dimension '%(maximumString)s' must be "
-                                     "greater than the start point '%(minimumString)s'." % locals())
-    
-    return minimumString, maximumString
-    
