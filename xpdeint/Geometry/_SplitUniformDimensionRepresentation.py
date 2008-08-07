a2ca119610c4226c71df33cba7901f420355e90d
grahamdennis <grahamdennis@41b19578-b77a-4e3b-9682-c8570d237623>
2008-08-07 01:42:28 +0000

    xpdeint: More files I forgot to add with the DCT/DST changes.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xmds/code/trunk/xpdeint@2079 41b19578-b77a-4e3b-9682-c8570d237623

---
+++ xpdeint/Geometry/_SplitUniformDimensionRepresentation.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+_SplitUniformDimensionRepresentation.py
+
+Created by Graham Dennis on 2008-07-30.
+Copyright (c) 2008 __MyCompanyName__. All rights reserved.
+"""
+
+from xpdeint.Geometry.DimensionRepresentation import DimensionRepresentation
+
+class _SplitUniformDimensionRepresentation(DimensionRepresentation):
+  def __init__(self, **KWs):
+    localKWs = self.extractLocalKWs(['range'], KWs)
+    DimensionRepresentation.__init__(self, **KWs)
+    
+    self._range = localKWs['range']
+  
+  def __eq__(self, other):
+    try:
+      return (DimensionRepresentation.__eq__(self, other) and
+              self._range == other._range)
+    except AttributeError:
+      return NotImplemented
+  
+  def _newInstanceDict(self):
+    result = DimensionRepresentation._newInstanceDict(self)
+    result.update({'range': self._range})
+    return result
+  
+  @property
+  def alternateLoopIndex(self):
+    return '_j_' + self.name
+  
+  def openLoop(self, loopingOrder):
+    if loopingOrder == self.LoopingOrder.MemoryOrder:
+      return self.openLoopMemoryOrder()
+    elif loopingOrder == self.LoopingOrder.StrictlyAscendingOrder:
+      return self.openLoopAscending()
+    else:
+      raise NotImplemented
+  
+  def closeLoop(self, loopingOrder):
+    if loopingOrder == self.LoopingOrder.MemoryOrder:
+      return self.closeLoopMemoryOrder()
+    elif loopingOrder == self.LoopingOrder.StrictlyAscendingOrder:
+      return self.closeLoopAscending()
+    else:
+      raise NotImplemented
+  
+
+
