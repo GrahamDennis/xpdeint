66db435e22c824b1b63980e4bf4b4e6ac0597ac6
grahamdennis <grahamdennis@a0a4e76c-ba24-0410-a01c-b269113d594b>
2009-06-26 07:03:36 +0000

    Clean up the compile command used for compiling simulations.
    Also turned off the RPATH compiler option on Mac OS X as it does nothing.
    
    git-svn-id: https://xmds.svn.sourceforge.net/svnroot/xmds/trunk/xpdeint@2309 a0a4e76c-ba24-0410-a01c-b269113d594b

--- xpdeint/Utilities.py
+++ xpdeint/Utilities.py
@@ -152,3 +152,16 @@ def symbolInString(string, xmlElement = None):
         return None
     
 
+def unique(seq, idfun=None):
+    # order preserving
+    if idfun is None:
+        def idfun(x): return x
+    seen = {}
+    result = []
+    for item in seq:
+        marker = idfun(item)
+        if marker in seen: continue
+        seen[marker] = 1
+        result.append(item)
+    return result
+
