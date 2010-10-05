a75fe73c479b10dfcdd45adccf4bf83ef08c4c33
grahamdennis <grahamdennis@41b19578-b77a-4e3b-9682-c8570d237623>
2010-10-05 07:03:38 +0000

    Began fixing stochastic testsuite and converting it to use noise_vectors.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xmds/code/trunk/xpdeint@2430 41b19578-b77a-4e3b-9682-c8570d237623

--- xpdeint/Features/_ErrorCheck.py
+++ xpdeint/Features/_ErrorCheck.py
@@ -8,6 +8,9 @@ Copyright (c) 2008 __MyCompanyName__. All rights reserved.
 """
 
 from xpdeint.Features._Feature import _Feature
+
+from xpdeint.Segments.Integrators.FixedStep import FixedStep
+from xpdeint.Operators.DeltaAOperator import DeltaAOperator
 from xpdeint.Vectors.NoiseVector import NoiseVector
 
 class _ErrorCheck (_Feature):
@@ -18,7 +21,15 @@ class _ErrorCheck (_Feature):
     for mg in self.getVar('momentGroups'):
       mg.processedVector.aliases.add('_%s_halfstep' % mg.outputField.name)
     
-    for noiseVector in [o for o in self.getVar('templates') if isinstance(o, NoiseVector)]:
-      if not noiseVector.static:
+    # When we have error checking, every noise vector used by a delta a operator in a fixed step integrator
+    # needs a '2' noise vector alias for generating two half-step noises and adding them.
+    if 'ErrorCheck' in self.getVar('features'):
+      noiseVectorsNeedingAlias = set()
+      # Loop over all fixed-step delta A operators ...
+      for deltaAOperator in [o for o in self.getVar('templates') if isinstance(o, DeltaAOperator) and isinstance(o.integrator, FixedStep)]:
+        # ... adding the noise vectors that are dependencies of that delta A operator
+        noiseVectorsNeedingAlias.update([v for v in deltaAOperator.dependencies if isinstance(v, NoiseVector)])
+        
+      for noiseVector in noiseVectorsNeedingAlias:
         noiseVector.aliases.add('_%s2' % noiseVector.id)
-      
+    
