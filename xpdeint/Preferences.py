44642f64d30a2c132002165fbc64e0ead8bd4cc0
grahamdennis <grahamdennis@41b19578-b77a-4e3b-9682-c8570d237623>
2012-01-16 10:07:02 +0000

    Environment variable XMDS_USER_DATA sets the xmds2 config path.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xmds/code/trunk/xpdeint@2491 41b19578-b77a-4e3b-9682-c8570d237623

--- xpdeint/Preferences.py
+++ xpdeint/Preferences.py
@@ -4,4 +4,7 @@ import os
 
 versionString = '2.1 "Happy Mollusc"'
 
-xpdeintUserDataPath = os.path.join(os.path.expanduser('~'), '.xmds')
+if 'XMDS_USER_DATA' in os.environ:
+    xpdeintUserDataPath = os.environ['XMDS_USER_DATA']
+else:
+    xpdeintUserDataPath = os.path.join(os.path.expanduser('~'), '.xmds')
