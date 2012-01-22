6f12be96bc9d47bf2ef17c056fa271b834d5a272
grahamdennis <grahamdennis@41b19578-b77a-4e3b-9682-c8570d237623>
2012-01-22 11:40:37 +0000

    Create script for mostly automated releases.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xmds/code/trunk/xpdeint@2509 41b19578-b77a-4e3b-9682-c8570d237623

---
+++ admin/create_release_version.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# Exit if anything fails
+set -o errexit
+
+# Run by typing ./create_release_version.sh in admin/ directory
+cd ..
+XMDS_VERSION=`python setup.py --version`
+
+# Create Version.py with subversion revision information
+cd xpdeint
+./version.sh
+cd ..
+
+if [ -d admin/staging ]; then 
+	rm -rf admin/staging;
+fi
+
+mkdir -p admin/staging/
+
+# Create a clean checkout
+svn export . admin/staging/xmds-${XMDS_VERSION}
+# Copy Version.py with revision information
+cp xpdeint/Version.py admin/staging/xmds-${XMDS_VERSION}/xpdeint
+
+cd admin/staging/xmds-${XMDS_VERSION}
+cd admin/userdoc-source
+# Build html docs
+make html
+# Copy built HTML docs into staging/ dir for later transfer to website
+cp -r ../../documentation ../../../
+# Build LaTeX docs
+make latex
+cd ../../documentation/latex
+# Actually make the PDF
+make all-pdf
+cd ../..
+
+# Compile Cheetah templates
+make
+
+# Clean up after waf
+make distclean
+
+cd ..
+tar -czf xmds-${XMDS_VERSION}.tar.gz xmds-${XMDS_VERSION}
+
+echo To release to sourceforge, execute the following commands.
+echo scp staging/xmds-${XMDS_VERSION}.tar.gz frs.sf.net:/home/frs/project/x/xm/xmds/
+echo "cd staging; scp -r documentation web.sf.net:/home/project-web/xmds/htdocs/"
