4201fcb32116a0b9385f5ea0474f99944dfcad16
grahamdennis <grahamdennis@41b19578-b77a-4e3b-9682-c8570d237623>
2012-01-23 10:34:40 +0000

    Added xmds logo to user documentation.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xmds/code/trunk/xpdeint@2513 41b19578-b77a-4e3b-9682-c8570d237623

--- admin/userdoc-source/conf.py
+++ admin/userdoc-source/conf.py
@@ -100,7 +100,7 @@ html_style = 'default.css'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-#html_logo = None
+html_logo = 'images/xmds_logo.png'
 
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
@@ -169,7 +169,7 @@ latex_documents = [
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
-#latex_logo = None
+latex_logo = 'images/xmds_logo.pdf'
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
