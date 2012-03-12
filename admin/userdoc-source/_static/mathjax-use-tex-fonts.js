4be7d951cee4667425a1193e5b99026cebc6ed3c
grahamdennis <grahamdennis@41b19578-b77a-4e3b-9682-c8570d237623>
2012-03-12 22:56:53 +0000

    Update MathJax config settings now that MathJax-2 is out.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xmds/code/trunk/xpdeint@2648 41b19578-b77a-4e3b-9682-c8570d237623

--- admin/userdoc-source/_static/mathjax-use-tex-fonts.js
+++ admin/userdoc-source/_static/mathjax-use-tex-fonts.js
@@ -6,8 +6,4 @@ MathJax.Hub.Config({
   },
 });
 
-MathJax.Hub.Queue(function () {
-if (document.location.hash) {setTimeout("document.location = document.location.hash",1)} 
-});
-
 MathJax.Ajax.loadComplete("http://www.xmds.org/_static/mathjax-use-tex-fonts.js");
