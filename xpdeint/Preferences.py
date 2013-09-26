de1d559b9e333b54ae5f3900792070bdb2a5b4d5
grahamdennis <grahamdennis@41b19578-b77a-4e3b-9682-c8570d237623>
2013-09-26 00:27:45 +0000

    Update version name.
    git-svn-id: svn+ssh://svn.code.sf.net/p/xmds/code/trunk/xpdeint@2866 41b19578-b77a-4e3b-9682-c8570d237623

--- xpdeint/Preferences.py
+++ xpdeint/Preferences.py
@@ -2,7 +2,7 @@
 # encoding: utf-8
 import os
 
-versionString = '2.1.4 "Happy Mollusc"'
+versionString = '2.1.4 "Well if this isn\'t nice, I don\'t know what is"'
 
 if 'XMDS_USER_DATA' in os.environ:
     xpdeintUserDataPath = os.environ['XMDS_USER_DATA']
